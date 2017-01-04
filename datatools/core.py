f1c4bc6034a446be64a205f1dd030bd20e9ad8de
Antonio Lima <anto87@gmail.com>
2017-01-04 13:52:16 +0000

    Add savendjson

--- datatools/core.py
+++ datatools/core.py
@@ -1,4 +1,5 @@
 import numpy as np
+import json
 
 def get_pdf(data, **kwargs):
     pdf, bin_edges = np.histogram(data, density=True, **kwargs)
@@ -9,5 +10,11 @@ def get_cdf(data, **kwargs):
     pdf, bin_edges = get_pdf(data, **kwargs)
     cdf = np.cumsum(pdf * np.diff(bin_edges))
     return cdf, bin_edges
+
+def save_ndjson(iterable, fname, json_fun=json.dumps, **dump_opts):
+    with open(fname, "w+") as fw:
+        for element in iterable:
+            line = json_fun(element, **dump_opts)
+            fw.write("{0}\n".format(line))
 
 
